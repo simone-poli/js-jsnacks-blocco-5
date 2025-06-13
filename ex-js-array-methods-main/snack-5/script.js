9ae6cea34d57b812eacb2b2eb2a6d882f9bcc542
simone-poli <polismn97@gmail.com>
2025-06-13 15:37:30 +0200

    snack5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,10 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const numeripari = nums.filter((numero) => {
+    if(numero % 2 === 0){
+        return true
+    }
+})
+console.log(numeripari)
 // Risultato: [2, 8, 4, 12]
