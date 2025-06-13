e0bf20b6b0bcb07ebcf9d4ba9433c60f61c07a9b
simone-poli <polismn97@gmail.com>
2025-06-13 15:23:24 +0200

    snack1

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,7 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+names.forEach((namesEL) =>{
+    console.log(namesEL)
+})
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
