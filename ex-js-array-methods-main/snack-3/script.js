34cc400ad519897b78c12df924e852461af8aa2f
simone-poli <polismn97@gmail.com>
2025-06-13 15:30:26 +0200

    snack3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,9 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+ const plus = numbers.map((numero) => {
+    return numero + 1
+})
+console.log(numbers)
+console.log(plus)
 // Risultato: [3, 9, 5, 8, 3, 88]
